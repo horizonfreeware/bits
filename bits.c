bfbabf9affb122dcd3a3c110fdb5629793e4879e
jpatrick5402 <jpatrick5402@gmail.com>
2025-04-29 08:28:05 -0400

    Adding more to raw

--- bits.c
+++ bits.c
@@ -33,17 +33,46 @@ int handle_error(char* message) {
 }
 
 // Func - print binary output
-void print_raw(FILE *file_pointer) {
+void print_raw(FILE *file_pointer_1, FILE *file_pointer_2) {
+  int lineCount = 1;
+  int charCount = 1;
   char *bitString;
   unsigned char byte;
-  while (fread(&byte, sizeof(byte), 1, file_pointer) == 1) {
+
+  while (fread(&byte, sizeof(byte), 1, file_pointer_1) == 1) {
     for (int i = 7; i >= 0; i--) {
       bitString = bitString + (byte >> i);
       printf("%d", (byte >> i) & 1); // Print bit
     }
   }
-  printf("\n");
-  fclose(file_pointer);
+
+  printf("\n\n");
+
+  while (fread(&byte, sizeof(byte), 1, file_pointer_2) == 1) {
+    if (byte == '\n') {
+      printf("\\n");
+      charCount = 1;
+      lineCount += 1;
+    }
+    else if (byte == '\t') {
+      printf("\\t");
+    }
+    else if (byte == '\r') {
+      printf("\\r");
+    }
+    else if (byte == ' ') {
+      printf("[space]");
+      charCount += 1;
+    }
+    else {
+      wchar_t unicode_char = byte;
+      printf("%lc", unicode_char); // Print Unicode Value
+      charCount += 1;
+    }
+  }
+
+  fclose(file_pointer_1);
+  fclose(file_pointer_2);
 }
 
 // Func - print detailed output
@@ -152,12 +181,14 @@ void print_pretty(FILE *file_pointer) {
 
 int main(int argc, char *argv[]) {
   FILE *fptr;
+  FILE *fptr2;
 
   // HandleErr user only inputs one argument
   if (argc <= 2) handle_error("Only one argument provided");
 
   // open file path input
   fptr = fopen(argv[argc - 1], "rb");
+  fptr2 = fopen(argv[argc - 1], "rb");
 
   // HandleErr file path invalid
   if(fptr == 0) handle_error("Path invalid");
@@ -166,7 +197,7 @@ int main(int argc, char *argv[]) {
   if (strcmp(argv[1], "-h") == 0)
     print_help();
   else if (strcmp(argv[1], "-r") == 0)
-    print_raw(fptr);
+    print_raw(fptr, fptr2);
   else if (strcmp(argv[1], "-v") == 0)
     print_verbose(fptr);
   else if (strcmp(argv[1], "-p") == 0)
